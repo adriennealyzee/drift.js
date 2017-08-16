990bb51f3debbcf3c190f2c8213a1f067966d569
Adrienne Tran <adrienne@adriennetran.com>
2017-08-16 11:03:50 -0400

    glow box

--- ti.js
+++ ti.js
@@ -1,35 +1,38 @@
+
+// Wiggle
 var driftHTML = $('.drift.wiggle').html();
 var driftHTML2 = '<span class="wiggleText wiggleStatic">' + driftHTML + '</span>';
 $('.drift.wiggle').html(driftHTML2);
 
 $('.drift.wiggle').on('click', function(e) {
-
-  // Resizing
   var target = $(this).find('.wiggleText');
-  console.log('target', target);
-  // target.data('original-size', $(this).css('fontSize'));
-  // var originalSize = target.data('original-size');
-  // $(this).animate({ fontSize: '+=2' });
-  // $(this).animate({ fontSize: originalSize });
-
   target.addClass('wiggleAnimate').delay(300).queue(function(next) {
     target.removeClass('wiggleAnimate');
     next();
   })
 
-  // $(this).addClass('animate').delay(300).queue(function(next) {
-  //   $(this).removeClass('animate');
-  //   next();
-  // })
-
 });
 
 // Sliding animation
-$('.slide').hover(function() {
-  $(this).addClass('slideAnimate')
-}, function() {
-  $(this).removeClass('slideAnimate');
+$('.slide').click(function() {
+  $(this).toggleClass('slideAnimate');
 });
 
+
+// Bounce
+$('.bounce').click(function() {
+  $(this).addClass('bounceAnimate').delay(300).queue(function(next) {
+    $(this).removeClass('bounceAnimate');
+    next();
+  });
+});
+
+// Glow
+$('.glow').click(function() {
+  $(this).addClass('glowAnimate').delay(300).queue(function(next) {
+    $(this).removeClass('glowAnimate');
+    next();
+  });
+});
 
 
---
+++ tidbits.js
@@ -0,0 +1,4 @@
+  // target.data('original-size', $(this).css('fontSize'));
+  // var originalSize = target.data('original-size');
+  // $(this).animate({ fontSize: '+=2' });
+  // $(this).animate({ fontSize: originalSize });
